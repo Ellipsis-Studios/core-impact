aeb613de91d9e69a8d51e6347f9edf24978d6c88
William Long <william@Williams-MacBook-Pro.local>
2021-03-04 01:47:25 -0500

    wrote header file for planet model

---
+++ source/CIPlanetModel.cpp
@@ -0,0 +1,9 @@
+//
+//  CIPlanetModel.cpp
+//  ShipDemo
+//
+//  Created by William Long on 3/4/21.
+//  Copyright © 2021 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#include <stdio.h>
