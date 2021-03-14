effcc0797c92b8c86e71c3c318f90bbaf9ec2b20
William Long <william@dhcp-vl2042-11635.redrover.cornell.edu>
2021-03-13 23:16:27 -0500

    added model class for game updates

---
+++ source/CIGameUpdate.cpp
@@ -0,0 +1,27 @@
+//
+//  CIGameUpdate.cpp
+//  CoreImpact
+//
+//  This class is used to store information associated with game updates between players.
+//
+//  Created by William Long on 3/13/21.
+//  Copyright © 2021 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#include "CIGameUpdate.h"
+
+void GameUpdate::dispose() {
+    _game_id.clear();
+    _stardust_sent.clear();
+    _planet.dispose();
+}
+
+bool GameUpdate::init(std::string gameId, int playerId, std::map<int, std::vector<StardustModel>> stardustSent, PlanetModel planet, int timestamp) {
+    _game_id = gameId;
+    _player_id = playerId;
+    _stardust_sent = stardustSent;
+    _planet = planet;
+    _timestamp = timestamp;
+}
+
+
