d699bcf7e0343f753c103e308d22b4a2d1aa5444
jy392 <jy392@cornell.edu>
2021-03-15 08:11:02 -0400

    better random color generation

--- source/CIColor.h
+++ source/CIColor.h
@@ -12,6 +12,8 @@
 #ifndef __CI_COLOR_H__
 #define __CI_COLOR_H__
 #include <cugl/cugl.h>
+#include <random>
+
 
 class CIColor {
 public:
@@ -31,6 +33,10 @@ public:
     
     /**
      * Get the Color4 represented by a particular CIColor value
+     * 
+     * @param CIColor Value 
+     * 
+     * @return Color4 representation of the CIColor Value
      */
     static cugl::Color4 getColor4(Value v) {
         cugl::Color4 color;
@@ -61,12 +67,21 @@ public:
         
         return color;
     }
-    
-    /**
-     * Get random CIColor Value.
+
+    /** 
+     * Returns a random CIColor Value for a stardust.
+     * 
+     * This method picks out of the first 5 CIColor values since they 
+     * are valid for stardust color. 
+     * 
+     * @return CIColor Value for a stardust 
      */
     static Value getRandomColor() {
-        return Value(rand() % 5); // pick out of first 5 colors 
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(0, 4); // pick out of first 5 colors
+        int rand = dist(mt);
+        return Value(rand);  
     }
 };
 
